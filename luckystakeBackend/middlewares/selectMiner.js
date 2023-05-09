3cb4777c53f44610dd241ce19a8fd3b7bf63b19f
Ganesh Thoutam <ganeshgb1024@gmail.com>
2023-05-10 01:46:16 +0530

    v2

--- luckystakeBackend/middlewares/selectMiner.js
+++ luckystakeBackend/middlewares/selectMiner.js
@@ -1,42 +1,46 @@
-  const list=require('./generateRandom');
-const tempDataModel = require('../Models/tempdata');
-const fun=require('../middlewares/AddBlock')
-const minermodel=require('../Models/miner')
-const sendResponse= async function(req,res,next)
-{
-       
-       //let remainingMiners=await tempDataModel.findOne({name:"lokya"})
-       //const arr=remainingMiners.priorityMiners;
-       //console.log(arr.length);
-       const data=req.body;
-        if(data.minerName==="empty")
-        {
-           // await tempDataModel.deleteMany({name:"lokya"})
+const list = require("./generateRandom");
+const tempDataModel = require("../Models/tempdata");
+const fun = require("../middlewares/AddBlock");
+const minermodel = require("../Models/miner");
+const sendResponse = async function (req, res, next) {
+  //let remainingMiners=await tempDataModel.findOne({name:"lokya"})
+  //const arr=remainingMiners.priorityMiners;
+  //console.log(arr.length);
+  const data = req.body;
+  if (data.minerName === "empty") {
+    // await tempDataModel.deleteMany({name:"lokya"})
 
-           next();
-        }
-        else
-        {
-           // const getMiner=await tempDataModel.findOne({name:"lokya"})
-           // const getMineraray=getMiner.priorityMiners;
-           /// const miner=getMineraray[getMineraray.length-1];
-           // console.log(miner);
-           const miner=data.minerName;
-           console.log(miner);
-            await minermodel.updateOne({name:miner},
-                {
-                    $inc:{stake:-50}
-                })
-                await minermodel.updateOne({name:miner},
-                    {
-                        $inc:{balance:-50}
-                    })
-            await tempDataModel.updateOne(
-                {name:"lokya"}, {$pop:{priorityMiners: 1}}
-               )
-            res.send("true");
+    next();
+  } else {
+    // const getMiner=await tempDataModel.findOne({name:"lokya"})
+    // const getMineraray=getMiner.priorityMiners;
+    /// const miner=getMineraray[getMineraray.length-1];
+    // console.log(miner);
+    const miner = data.minerName;
+    console.log(miner);
 
-        }
-    
-}
-module.exports=sendResponse;
+    const miners = await minermodel.find();
+    const minersSize = miners.length;
+    const freezeCount = minersSize / 2;
+
+    await minermodel.updateOne(
+      { name: miner },
+      {
+        $inc: { stake: -50 },
+        $set: { freazeCount: freezeCount + 1 },
+      }
+    );
+    await minermodel.updateOne(
+      { name: miner },
+      {
+        $inc: { balance: -50 },
+      }
+    );
+    await tempDataModel.updateOne(
+      { name: "lokya" },
+      { $pop: { priorityMiners: 1 } }
+    );
+    res.send("true");
+  }
+};
+module.exports = sendResponse;
